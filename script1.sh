4fed85b1b0362e8f4b9c3c882d20b5ec54ccd757
HMM56 <hecmarmun@hotmail.com>
2025-10-29 19:54:51 +0100

    script1 creacion de la VPC funcional.

--- script1.sh
+++ script1.sh
@@ -12,3 +12,14 @@ aws ec2 modify-vpc-attribute \
     --enable-dns-hostnames "{\"Value\":true}"
 
 
+# Creación de la subred. Le damos 4 bits para subred.
+SUB_ID=$(aws ec2 create-subnet \
+    --vpc-id $VPC_ID \
+    --cidr-block 192.168.0.0/28 \
+    --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=Hector-Subred}]' \
+    --query Subnet.SubnetId --output text)
+
+# Mostrar el ID de la Subred.
+echo $SUB_ID
+
+aws ec2 modify-subnet-attribute --subnet-id $SUB_ID --map-public-ip-on-launch 
