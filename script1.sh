3400def56b5e94759011a762a9644a6ff41f3f92
HMM56 <hecmarmun@hotmail.com>
2025-10-29 19:19:24 +0100

    script1 creacion de la VPC funcional.

--- script1.sh
+++ script1.sh
@@ -1,3 +1,14 @@
-aws ec2 create-vpc --cidr-block 192.168.0.0/24 \
-    --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=MyVpc}]' \
-    --query Vpc.VpcId --output text
+# Creación VPC con salida de la ID.
+VPC_ID=$(aws ec2 create-vpc --cidr-block 192.168.0.0/24 \
+    --tag-specifications 'ResourceType=vpc,Tags=[{Key=Name,Value=Hector-VPC}]' \
+    --query Vpc.VpcId --output text)
+
+# Mostrar el ID de la VPC creada.
+echo $VPC_ID
+
+# Habilitar el DNS en la VPC.
+aws ec2 modify-vpc-attribute \
+    --vpc-id $VPC_ID \
+    --enable-dns-hostnames "{\"Value\":true}"
+
+
